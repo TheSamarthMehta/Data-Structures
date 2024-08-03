afc1ed0c968e3f572b68a56cc48feb43db81be06
TheSamarthMehta <160031551+TheSamarthMehta@users.noreply.github.com>
2024-08-03 20:22:02 +0530

    Delete Arrays directory

--- Arrays/Labno_01_02/DemoArea.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class DemoArea{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double radius = sc.nextInt();
-        double Area = 3.14 * radius * radius;
-        System.out.println("Area Of Circle Is : " + Area);
-    }
-}
